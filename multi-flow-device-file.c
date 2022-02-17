78c94393a706d12975024daf3e2b53249d619842
Simone Tiberi <tib.sim.98@gmail.com>
2022-02-17 12:23:47 +0100

    Impostato il range corretto di minor numbers e cambiati alcuni dettagli minori sul file del modulo (e.g. aggiunta della descrizione)

--- multi-flow-device-file.c
+++ multi-flow-device-file.c
@@ -7,14 +7,13 @@
 #include <linux/pid.h>
 #include <linux/tty.h>
 #include <linux/version.h>
-#include <linux/signal_types.h>
-#include <linux/syscalls.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Simone Tiberi email: simone.tiberi.98@gmail.com");
+MODULE_AUTHOR("Simone Tiberi <simone.tiberi.98@gmail.com>");
+MODULE_DESCRIPTION("Multi flow device file");
 
 #define MODNAME "[SOA-PROJECT]"
-#define DEVICE "multi-flow"
+#define DEVICE_NAME "multi-flow"
 
 /* Macro for the retrieve of major and minor code */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
@@ -30,51 +29,50 @@ int major;
 module_param(major, int, 0660);
 
 /* Prototypes of driver operations */
-static int procctl_open(struct inode *, struct file *);
-static int procctl_release(struct inode *, struct file *);
-static ssize_t procctl_write(struct file *, const char __user *, size_t, loff_t *);
+static int mfdf_open(struct inode *, struct file *);
+static int mfdf_release(struct inode *, struct file *);
+static ssize_t mfdf_write(struct file *, const char __user *, size_t, loff_t *);
 
-static int procctl_open(struct inode *inode, struct file *file)
+static int mfdf_open(struct inode *inode, struct file *file)
 {
-    printk("%s Thread %d has called an open on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE ,get_major(file), get_minor(file));
+    printk("%s Thread %d has called an open on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE_NAME ,get_major(file), get_minor(file));
     return 0;
 }
 
-static int procctl_release(struct inode *inode, struct file *file)
+static int mfdf_release(struct inode *inode, struct file *file)
 {
-    printk("%s Thread %d has called an release on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE ,get_major(file), get_minor(file));
+    printk("%s Thread %d has called an release on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE_NAME ,get_major(file), get_minor(file));
     return 0;
 }
 
-static ssize_t procctl_write(struct file * filp, const char __user * buff, size_t len, loff_t *off) 
+static ssize_t mfdf_write(struct file * filp, const char __user * buff, size_t len, loff_t *off) 
 {
-    printk("%s Thread %d has called an write on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE ,get_major(filp), get_minor(filp));
+    printk("%s Thread %d has called an write on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE_NAME ,get_major(filp), get_minor(filp));
     return len;
 }
 
 /* Driver for multi-flow-device-file */
 static struct file_operations fops = {
     .owner   = THIS_MODULE,
-    .open    = procctl_open,
-    .release = procctl_release,
-    .write   = procctl_write
+    .open    = mfdf_open,
+    .release = mfdf_release,
+    .write   = mfdf_write
 };
 
 int init_module(void)
 {
-    major = register_chrdev(0, DEVICE, &fops);
- 
+    major = __register_chrdev(0, 0, 128, DEVICE_NAME, &fops);
     if (major < 0) {
-        printk("Registering process controller device failed\n");
+        printk("Registering multi-flow device file failed\n");
         return major;
     }
 
-    printk(KERN_INFO "Process controller device registered, it is assigned major number %d\n", major);
+    printk(KERN_INFO "Multi flow device file registered (MAJOR number: %d)\n", major);
     return 0;
 }
 
 void cleanup_module(void)
 {
-    unregister_chrdev(major, DEVICE);
-    printk(KERN_INFO "Process controller device unregistered, it was assigned major number %d\n", major);
+    unregister_chrdev(major, DEVICE_NAME);
+    printk(KERN_INFO "Multi flow device file unregistered (MAJOR number: %d)\n", major);
 }
