662ce3b173fa36b4a0beccabb6fd0618e138184a
Simone Tiberi <tib.sim.98@gmail.com>
2022-02-19 17:41:43 +0100

    Implementato il meccanismo di pipe

--- multi-flow-device-file.c
+++ multi-flow-device-file.c
@@ -13,9 +13,11 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Simone Tiberi <simone.tiberi.98@gmail.com>");
 MODULE_DESCRIPTION("Multi flow device file");
 
-#define MODNAME "[SOA-PROJECT]"   // Module name, useful for debug printk
-#define DEVICE_NAME "multi-flow"  // Device name, useful for debug printk
-#define MINORS (128)              // Number of minors available
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MODNAME "[SOA-PROJECT]"     // Module name, useful for debug printk
+#define DEVICE_NAME "multi-flow"    // Device name, useful for debug printk
+#define MINORS (128)                // Number of minors available
+#define BUFSIZE (10)         // Size of buffer
 
 /* Macro for the retrieve of major and minor code from session */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
@@ -34,7 +36,8 @@ module_param(major, int, 0660);
 struct device_state {
     struct mutex synchronizer;
     char *data;
-    int valid_offset;
+    int read_offset;
+    int write_offset;
 } devs[MINORS];
 
 /* Prototypes of driver operations */
@@ -57,55 +60,92 @@ static int mfdf_release(struct inode *inode, struct file *file)
 
 static ssize_t mfdf_write(struct file * filp, const char __user * buff, size_t len, loff_t *off)
 {
-    int residual;
-    int minor = get_minor(filp);
-    struct device_state *the_device = devs + minor;
-
+    int minor, residual;
+    size_t to_end_length, from_start_length;
+    struct device_state *the_device;
     printk("%s Thread %d has called a write on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE_NAME ,get_major(filp), get_minor(filp));
 
-    mutex_lock(&(the_device->synchronizer));
+    minor = get_minor(filp);
+    the_device = devs + minor;
 
-    if (*off > PAGE_SIZE) { // if offset is invalid return error (errno: ENOSPC)
+    mutex_lock(&(the_device->synchronizer));
+    to_end_length = MIN(len, BUFSIZE - (the_device->write_offset));
+    residual = copy_from_user(the_device->data + the_device->write_offset, buff, to_end_length);
+    if (unlikely(residual) != 0) {
         mutex_unlock(&(the_device->synchronizer));
-        return -ENOSPC;
+        printk("%s Write operation requested by %d on %s device failed", MODNAME, current->pid, DEVICE_NAME);
+        return -EBADFD;
     }
 
-    if((PAGE_SIZE - *off) < len) // if length is greater than available space, adjust it
-        len = PAGE_SIZE - *off;
+    printk("%s First part of write operation requested by %d on %s device succeded (from %d to %ld)",
+           MODNAME, current->pid, DEVICE_NAME, the_device->write_offset, the_device->write_offset + to_end_length);
+    the_device->write_offset += to_end_length;
 
-    // Write data and update offset
-    residual = copy_from_user(&(the_device->data[*off]),buff,len);
-    *off += (len - residual);
-    the_device->valid_offset = *off;
+    from_start_length = MIN((len - to_end_length), the_device->read_offset);
+    if (from_start_length > 0) {
+        residual = copy_from_user(the_device->data, buff + to_end_length, from_start_length);
+        if (unlikely(residual) != 0) {
+            mutex_unlock(&(the_device->synchronizer));
+            printk("%s Write operation requested by %d on %s device failed", MODNAME, current->pid, DEVICE_NAME);
+            return -EBADFD;
+        }
 
+        printk("%s Second part of write operation requested by %d on %s device succeded (from 0 to %ld)",
+               MODNAME, current->pid, DEVICE_NAME, from_start_length);
+        the_device->write_offset = from_start_length;
+    }
     mutex_unlock(&(the_device->synchronizer));
 
-    return len - residual;
+    return to_end_length + from_start_length;
 }
 
 static ssize_t mfdf_read(struct file *filp, char *buff, size_t len, loff_t *off)
 {
-    int residual;
-    int minor = get_minor(filp);
-    struct device_state *the_device = devs + minor;
+    int minor, residual;
+    size_t to_end_length, from_start_length;
+    struct device_state *the_device;
 
     printk("%s Thread %d has called a read on %s device [MAJOR: %d, minor: %d]", MODNAME, current->pid, DEVICE_NAME ,get_major(filp), get_minor(filp));
 
+    minor = get_minor(filp);
+    the_device = devs + minor;
+
     mutex_lock(&(the_device->synchronizer));
 
-    if(*off > the_device->valid_offset) {
-        mutex_unlock(&(the_device->synchronizer));
-        return 0;
+    if(the_device->write_offset >= the_device->read_offset) {
+        to_end_length = MIN(len, (the_device->write_offset - the_device->read_offset));
+        from_start_length = 0;
+    } else {
+        to_end_length = MIN(len, (BUFSIZE - the_device->read_offset));
+        from_start_length = MIN(len - to_end_length, the_device->write_offset);
     }
-    if((the_device->valid_offset - *off) < len)
-        len = the_device->valid_offset - *off;
 
-    residual = copy_to_user(buff,&(the_device->data[*off]),len);
+    residual = copy_to_user(buff, the_device->data + the_device->read_offset, to_end_length);
+    if (unlikely(residual) != 0) {
+        mutex_unlock(&(the_device->synchronizer));
+        printk("%s Read operation requested by %d on %s device failed", MODNAME, current->pid, DEVICE_NAME);
+        return -EBADFD;
+    }
 
-    *off += (len - residual);
+    printk("%s First part of read operation requested by %d on %s device succeded (from %d to %ld)",
+           MODNAME, current->pid, DEVICE_NAME, the_device->read_offset, the_device->read_offset + to_end_length);
+    the_device->read_offset += to_end_length;
+
+    if (from_start_length > 0) {
+        residual = copy_to_user(buff + to_end_length, the_device->data, from_start_length);
+        if (unlikely(residual) != 0) {
+            mutex_unlock(&(the_device->synchronizer));
+            printk("%s Read operation requested by %d on %s device failed", MODNAME, current->pid, DEVICE_NAME);
+            return -EBADFD;
+        }
+
+        printk("%s Second part of read operation requested by %d on %s device succeded (from 0 to %ld)",
+               MODNAME, current->pid, DEVICE_NAME, from_start_length);
+        the_device->read_offset = from_start_length;
+    }
     mutex_unlock(&(the_device->synchronizer));
 
-    return len - residual;
+    return to_end_length + from_start_length;
 }
 
 /* Driver for multi-flow-device-file */
@@ -121,22 +161,24 @@ static struct file_operations fops = {
  * Initialize devices metadata and return the number of devices initialized
  */
 static int init_devices(void) {
-    int i;
-    for (i=0; i<MINORS; ++i) {
-        mutex_init(&(devs[i].synchronizer));
-        devs[i].data = (char *)get_zeroed_page(GFP_KERNEL);
-        if (devs[i].data == NULL) break;
+    int index;
+    for (index=0; index<MINORS; ++index) {
+        mutex_init(&(devs[index].synchronizer));
+        devs[index].read_offset = 0;
+        devs[index].write_offset = 0;
+        devs[index].data = (char *)get_zeroed_page(GFP_KERNEL);
+        if (unlikely(devs[index].data == NULL)) break;
     }
 
-    return i;
+    return index;
 }
 
 int init_module(void)
 {
-    int tot = init_devices();
-    if (tot != MINORS) {
-        for(; tot>=0; --tot)
-            free_page((unsigned long)devs[tot].data);
+    int last_index = init_devices();
+    if (unlikely(last_index != MINORS)) {
+        for (; last_index >= 0; --last_index)
+            free_page((unsigned long)devs[last_index].data);
         return -ENOMEM;
     }
 
