f8a689795755fe92d5e06bae8fb27ce336d0940f
Simone Tiberi <tib.sim.98@gmail.com>
2022-02-25 16:11:23 +0100

    Modificata la struttura della directory user

--- user/src/test.c
+++ user/src/test.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <mfdf/user.h>
+#include "user.h"
 
 int main()
 {
@@ -16,7 +16,13 @@ int main()
 
     mfdf_set_read_modality(fd,BLOCK);
     mfdf_set_write_modality(fd,BLOCK);
-    printf("Read return value: %ld\n", mfdf_prio_gets(fd, LOW_PRIO, buff, 10));
+
+    memset(buff, 0x0, 4096);
+    memset(buff, 0x41, 4094);
+    printf("Write return value: %ld\n", mfdf_printf_low(fd, buff));
+    printf("Write return value: %ld\n", mfdf_printf_low(fd, "Ciao ciao ciao ciao"));
+
+    // printf("Read return value: %ld\n", mfdf_prio_gets(fd, LOW_PRIO, buff, 10));
 
     // printf("Write return value: %ld\n", mfdf_printf_low(fd, "Hello everyone I'm %s and I'm writing on file descriptor %d", "Simone", fd));
     // printf("Write return value: %ld\n", mfdf_printf_high(fd, "This is a very important message"));
@@ -27,5 +33,5 @@ int main()
     //
     // memset(buff, 0x0, 4096);
     // printf("Read return value: %ld\n", mfdf_prio_gets(fd, HIGH_PRIO, buff, 4096));
-    printf("This is what I read: \"%s\"\n", buff);
+    // printf("This is what I read: \"%s\"\n", buff);
 }
