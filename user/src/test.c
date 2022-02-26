ad2e77d967b944855f36e41417eea4ad65221c5a
Simone Tiberi <tib.sim.98@gmail.com>
2022-02-26 12:44:43 +0100

    Modificato il file di test per verificare il corretto funzionamento delle modifiche effettuate

--- user/src/test.c
+++ user/src/test.c
@@ -16,13 +16,13 @@ int main()
 
     mfdf_set_read_modality(fd,BLOCK);
     mfdf_set_write_modality(fd,BLOCK);
+    printf("Read return value: %ld\n", mfdf_prio_gets(fd, LOW_PRIO, buff, 10));
 
-    memset(buff, 0x0, 4096);
-    memset(buff, 0x41, 4094);
-    printf("Write return value: %ld\n", mfdf_printf_low(fd, buff));
-    printf("Write return value: %ld\n", mfdf_printf_low(fd, "Ciao ciao ciao ciao"));
+    // memset(buff, 0x0, 4096);
+    // memset(buff, 0x41, 4094);
+    // printf("Write return value: %ld\n", mfdf_printf_low(fd, buff));
+    // printf("Write return value: %ld\n", mfdf_printf_low(fd, "Ciao ciao ciao ciao"));
 
-    // printf("Read return value: %ld\n", mfdf_prio_gets(fd, LOW_PRIO, buff, 10));
 
     // printf("Write return value: %ld\n", mfdf_printf_low(fd, "Hello everyone I'm %s and I'm writing on file descriptor %d", "Simone", fd));
     // printf("Write return value: %ld\n", mfdf_printf_high(fd, "This is a very important message"));
