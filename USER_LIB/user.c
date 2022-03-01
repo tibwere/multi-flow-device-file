b9108da09bd3cc84cc9ad3294850b4d50ccb64d9
Simone Tiberi <tib.sim.98@gmail.com>
2022-03-01 17:57:02 +0100

    Corretta l'indentazione della libreria user.c

--- USER_LIB/user.c
+++ USER_LIB/user.c
@@ -8,28 +8,26 @@
 
 ssize_t mfdf_prio_printf(int fd, int prio, const char *restrict format, ...)
 {
-    ssize_t ret;
-    char buff[4096];
+        ssize_t ret;
+        char buff[4096];
 
-    memset(buff, 0x0, 4096);
+        memset(buff, 0x0, 4096);
 
-    if(prio != KEEP_PRIO)
-        if (mfdf_set_priority(fd, prio))
-            return -1;
+        if(prio != KEEP_PRIO && mfdf_set_priority(fd, prio))
+                return -1;
 
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buff, 4096, format, args);
-    ret = write(fd,buff,strlen(buff));
-    va_end(args);
-    return ret;
+        va_list args;
+        va_start(args, format);
+        vsnprintf(buff, 4096, format, args);
+        ret = write(fd,buff,strlen(buff));
+        va_end(args);
+        return ret;
 }
 
 ssize_t mfdf_prio_gets(int fd, int prio, char *buff, size_t len)
 {
-    if(prio != KEEP_PRIO)
-        if (mfdf_set_priority(fd, prio))
-            return -1;
+        if(prio != KEEP_PRIO && mfdf_set_priority(fd, prio))
+                return -1;
 
-    return read(fd, buff, len);
+        return read(fd, buff, len);
 }
