8d005134e87686b7e2f0292a8c35794b082b918b
Simone Tiberi <tib.sim.98@gmail.com>
2022-03-06 19:41:24 +0100

    Resa meno ambigua la libreria utente

--- USER_LIB/user.c
+++ USER_LIB/user.c
@@ -8,19 +8,15 @@
  * Printf-like version of the write on the multi-flow device file
  *
  * @fd:     file descriptor
- * @prio:   priority indicator (LOW_PRIO or HIGH_PRIO)
  * @format: format string (as in printf)
  * @...:    arguments va_list
  */
-ssize_t mfdf_prio_printf(int fd, int prio, const char *restrict format, ...)
+ssize_t mfdf_printf(int fd, const char *restrict format, ...)
 {
         ssize_t ret;
         char buff[4096];
 
         memset(buff, 0x0, 4096);
-
-        if(prio != KEEP_PRIO && mfdf_set_priority(fd, prio))
-                return -1;
 
         va_list args;
         va_start(args, format);
@@ -28,21 +24,4 @@ ssize_t mfdf_prio_printf(int fd, int prio, const char *restrict format, ...)
         ret = write(fd,buff,strlen(buff));
         va_end(args);
         return ret;
-}
-
-
-/**
- * Reading wrapper from multi-flow device file
- *
- * @fd:   file descriptor
- * @prio: priority indicator (LOW_PRIO or HIGH_PRIO)
- * @buff: buffer to store the read data
- * @len:  number of bytes required to read
- */
-ssize_t mfdf_prio_gets(int fd, int prio, char *buff, size_t len)
-{
-        if(prio != KEEP_PRIO && mfdf_set_priority(fd, prio))
-                return -1;
-
-        return read(fd, buff, len);
 }
--- USER_LIB/user.h
+++ USER_LIB/user.h
@@ -15,27 +15,20 @@
  */
 
 
-#define KEEP_PRIO (-1)
-#define MFDF_READ_ONLY (O_RDONLY)
+#define KEEP_PRIO       (-1)
+#define MFDF_READ_ONLY  (O_RDONLY)
 #define MFDF_WRITE_ONLY (O_WRONLY)
 #define MFDF_READ_WRITE (O_RDWR)
 
-ssize_t mfdf_prio_printf(int, int, const char *restrict, ...);
-ssize_t mfdf_prio_gets(int, int, char *, size_t);
+ssize_t mfdf_printf(int, const char *restrict, ...);
 
-#define mfdf_open(path, flags) open(path, flags)
-#define mfdf_close(fd) close(fd)
-#define mfdf_set_priority(fd,level) ioctl(fd, MFDF_IOCTL_SET_PRIO, level)
-#define mfdf_set_read_modality(fd,modality) ioctl(fd, MFDF_IOCTL_SET_RMOD, modality)
-#define mfdf_set_write_modality(fd,modality) ioctl(fd, MFDF_IOCTL_SET_WMOD, modality)
-#define mfdf_set_timeout(fd,timeout) ioctl(fd, MFDF_IOCTL_SET_TOUT, timeout)
-
-#define mfdf_printf(fd, format,...) mfdf_prio_printf(fd, KEEP_PRIO, format, ##__VA_ARGS__)
-#define mfdf_printf_low(fd, format,...) mfdf_prio_printf(fd, LOW_PRIO, format, ##__VA_ARGS__)
-#define mfdf_printf_high(fd, format,...) mfdf_prio_printf(fd, HIGH_PRIO, format, ##__VA_ARGS__)
-
-#define mfdf_gets(fd, buff, len) mfdf_prio_gets(fd, KEEP_PRIO, buff, len)
-#define mfdf_gets_low(fd, buff, len) mfdf_prio_gets(fd, LOW_PRIO, buff, len)
-#define mfdf_gets_high(fd, buff, len) mfdf_prio_gets(fd, HIGH_PRIO, buff, len)
+#define mfdf_open(path, flags)                  open(path, flags)
+#define mfdf_close(fd)                          close(fd)
+#define mfdf_set_priority(fd, level)            ioctl(fd, MFDF_IOCTL_SET_PRIO, level)
+#define mfdf_set_read_modality(fd, modality)    ioctl(fd, MFDF_IOCTL_SET_RMOD, modality)
+#define mfdf_set_write_modality(fd, modality)   ioctl(fd, MFDF_IOCTL_SET_WMOD, modality)
+#define mfdf_set_timeout(fd, timeout)           ioctl(fd, MFDF_IOCTL_SET_TOUT, timeout)
+#define mfdf_read(fd, buff, size)               read(fd, buff, size)
+#define mfdf_write(fd, buff, size)              write(fd, buff, size)
 
 #endif // !__MFDF_H__
